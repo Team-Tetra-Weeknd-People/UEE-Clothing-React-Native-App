d55b10d72a576c2c2ff2ccf888cd601106d294a5
IT21008382 <it21008382@my.sliit.lk>
2023-10-23 22:37:55 +0530

    Process Manager Journey page created

---
+++ frontend/screens/ProcessManager/screens/ProcessManagerJourney.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Button, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+const ProcessManagerJourney = () => {
+    return (
+        <View>
+            <Text>Process Manager Journey Screen</Text>
+        </View>
+    );
+};
+
+export default ProcessManagerJourney;
