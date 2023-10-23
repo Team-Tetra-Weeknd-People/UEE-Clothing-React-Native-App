9882ccee97bb24a8da64c34561463a1e3e6b9642
IT21008382 <it21008382@my.sliit.lk>
2023-10-23 22:38:07 +0530

    Process Manager Orders page created

---
+++ frontend/screens/ProcessManager/screens/ProcessManagerOrders.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Button, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+const ProcessManagerOrders = () => {
+    return (
+        <View>
+            <Text>Process Manager Orders Screen</Text>
+        </View>
+    );
+};
+
+export default ProcessManagerOrders;
